e605fde5758c953c746795eb15871d6b306a233b
Hibatullah <hiba.shabbir.r@gmail.com>
2023-10-21 18:13:12 +0500

    Commit

--- app/src/main/java/com/example/a2_20l1271/MainActivity.kt
+++ app/src/main/java/com/example/a2_20l1271/MainActivity.kt
@@ -3,13 +3,6 @@ package com.example.a2_20l1271
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.a2_20l1271.ui.theme.A2_20L1271Theme
 
 class MainActivity : ComponentActivity() {
